6d1ef163051a25fef9c047a2eaa43136a73928c2
Gilad Shanan <giladshanan@gmail.com>
2017-11-01 12:03:14 -0500

    update variable scope and custom validation callbacks to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,5 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     redirect_to root_path
-    # redirect_to "https://www.spotify.com/us/logout" # need to figure out how to get back to users (maybe need ajax?)
   end
 end
